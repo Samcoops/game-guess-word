bbecede1b2bac91005a0ffdeb5d73e2a393dbb3f
igornedopolz <82241351+igornedopolz@users.noreply.github.com>
2024-08-04 19:18:08 +0200

    navbar text

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>navbar</div>
+    <div className='w-full h-[100px] flex justify-center items-center'>
+      <div className='text-4xl text-white'>THE WORDGUESSR GAME</div>
+    </div>
   )
 }
