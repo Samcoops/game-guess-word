bf648f7e343da78448eab002657ce7b792dae37d
igornedopolz <82241351+igornedopolz@users.noreply.github.com>
2024-08-04 20:23:07 +0200

    +game page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,7 +3,7 @@
 export default function GamePage() {
   return (
     <div>
-      GamePage
+      <div className="absolute top-[300px] rounded-r-3xl h-[75px] w-[400px] text-3xl flex items-center justify-center bg-[#020C1A]">{`Gamelevel : {hard}`} </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#09111F]`}>
+      <body className={`${inter.className} bg-[#0b1527]`}>
         <Navbar/>
         
         {children}
