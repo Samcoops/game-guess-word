5423181cc0f675cfab9df03ef6316cfb4e9f5b47
igornedopolz <82241351+igornedopolz@users.noreply.github.com>
2024-08-04 21:06:32 +0200

    + hover button

--- src/components/HandleGameButton.tsx
+++ src/components/HandleGameButton.tsx
@@ -24,8 +24,11 @@ export default function Button({ level }: { level: tGameLevel }) {
         <div>
             <button
                 onClick={() => handleGame(level)}
-                className='flex justify-center items-center w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl mx-10 my-20'
-            >
+                className={`flex justify-center items-center w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl mx-10 my-20 transition-all duration-200
+                ${level === 'easy' ?'hover:bg-green-500 hover:text-white' : 
+                  level === 'medium' ?'hover:bg-yellow-500 hover:text-white' :
+                  level === 'hard' ?'hover:bg-red-500 hover:text-white' : '' 
+                }`}>
                 <span>{level}</span>
             </button>
         </div>
