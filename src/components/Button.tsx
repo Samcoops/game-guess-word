b1b2330c1c4d3c6725a080fc71d53b2f9520f5a5
igornedopolz <82241351+igornedopolz@users.noreply.github.com>
2024-08-04 19:53:38 +0200

    +button to link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 import { tGameLevel } from '@/types'
+import Link from 'next/link'
 import React from 'react'
+
 
 export default function Button({text}: {text: tGameLevel}) {
   return (
     <div>
-        <button className='w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl my-10'>
+        <Link href={'/game'} className='flex justify-center items-center w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl my-10'>
             <span>{text}</span>
-        </button>
+        </Link>
     </div>
   )
 }
