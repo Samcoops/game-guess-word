8e6ebc283ef9981b13970c1911d6ad07dd4656b7
igornedopolz <82241351+igornedopolz@users.noreply.github.com>
2024-08-04 19:46:42 +0200

    +buttons home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full">
+    <main className="h-screen w-full flex flex-col justify-center items-center">
         <Button text="easy"/>
+        <Button text="medium"/>
+        <Button text="hard"/>
     </main>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Button({text}: {text: tGameLevel}) {
   return (
     <div>
-        <button className='w-10 h-10 text-black bg-white'>
+        <button className='w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl my-10'>
             <span>{text}</span>
         </button>
     </div>
