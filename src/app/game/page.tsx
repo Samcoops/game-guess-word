8706ac4c749de4d9c6cd5ff05a6abb8ebb24a426
Samcoops <146433803+Samcoops@users.noreply.github.com>
2024-08-04 18:30:17 +0200

    +

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,12 @@
-import { handleGame } from '@/lib/functions/handleGame';
 
 export default function GamePage() {
-  return (
-    <div>
-      <div className="absolute top-[300px] rounded-r-3xl h-[75px] w-[400px] text-3xl flex items-center justify-center bg-[#020C1A]">{`Gamelevel : {hard}`} </div>
-    </div>
-  )
+
+
+    return (
+        <div>
+            <div className='absolute top-[300px] rounded-r-3xl h-[75px] w-[400px] text-3xl flex items-center justify-center bg-[#020C1A]'>
+                {`Gamelevel : {hard}`}{' '}
+            </div>
+        </div>
+    );
 }
