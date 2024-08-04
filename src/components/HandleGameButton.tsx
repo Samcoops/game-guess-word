4271e23aa887d28ac4cef117b5b2b89533645a33
Samcoops <146433803+Samcoops@users.noreply.github.com>
2024-08-04 19:40:44 +0200

    +

--- src/components/HandleGameButton.tsx
+++ src/components/HandleGameButton.tsx
@@ -9,15 +9,12 @@ export default function Button({ level }: { level: tGameLevel }) {
 
     const handleGame = (level: tGameLevel): void => {
         const word = getWordToGuess(level);
-        console.log(word);
-        
-        cookie.set('game', 'true');
-        cookie.set('game_word', word.word);
-        cookie.set('game_hints_count', '0');
+        console.log('Game created and added to cookie');
 
+        cookie.set('word_id', word.id.toString());
+        cookie.set('hints_count', '0');
 
         router.push('/game');
-
     };
 
     return (
@@ -25,10 +22,16 @@ export default function Button({ level }: { level: tGameLevel }) {
             <button
                 onClick={() => handleGame(level)}
                 className={`flex justify-center items-center w-[200px] h-[50px] text-black bg-white rounded-3xl text-xl mx-10 my-20 transition-all duration-200
-                ${level === 'easy' ?'hover:bg-green-500 hover:text-white' : 
-                  level === 'medium' ?'hover:bg-yellow-500 hover:text-white' :
-                  level === 'hard' ?'hover:bg-red-500 hover:text-white' : '' 
-                }`}>
+                ${
+                    level === 'easy'
+                        ? 'hover:bg-green-500 hover:text-white'
+                        : level === 'medium'
+                        ? 'hover:bg-yellow-500 hover:text-white'
+                        : level === 'hard'
+                        ? 'hover:bg-red-500 hover:text-white'
+                        : ''
+                }`}
+            >
                 <span>{level}</span>
             </button>
         </div>
---
+++ src/lib/functions/getWordById.ts
@@ -0,0 +1,3 @@
+export function getWordById(id: number){
+    
+}
